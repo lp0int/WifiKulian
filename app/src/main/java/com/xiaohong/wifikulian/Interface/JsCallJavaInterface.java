7e05b68d05417d39fadccf7098a165cef3ebd909
Lpoint <xiaoawl23@hotmail.com>
2017-02-10 16:09:00 +0800

    edit JsCallJavaInterface

--- app/src/main/java/com/xiaohong/wifikulian/Interface/JsCallJavaInterface.java
+++ app/src/main/java/com/xiaohong/wifikulian/Interface/JsCallJavaInterface.java
@@ -1,8 +1,14 @@
 package com.xiaohong.wifikulian.Interface;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.webkit.JavascriptInterface;
 
+import com.xiaohong.wifikulian.Variable;
+import com.xiaohong.wifikulian.utils.DialogUtils;
 import com.xiaohong.wifikulian.utils.PhoneInfo;
+import com.xiaohong.wifikulian.utils.Utils;
 
 public class JsCallJavaInterface {
 
@@ -10,4 +16,32 @@ public class JsCallJavaInterface {
     public String GetUserPhone() {
         return PhoneInfo.PHONENUM();
     }
+
+    @JavascriptInterface
+    public void LogMsg(String tag, String info) {
+        Log.i(tag, info);
+    }
+
+    @JavascriptInterface
+    public void ToastMsg(final String info){
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                Utils.showToastStr(Variable.BASECONTEXT,info);
+            }
+        });
+    }
+
+    @JavascriptInterface
+    public void DialogMsg(final String title,final String info){
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                DialogUtils.getDialog(Variable.BASECONTEXT,title,info,"",null,"确认",null).setCancelable(true).show();
+            }
+        });
+    }
+
+
+
 }
