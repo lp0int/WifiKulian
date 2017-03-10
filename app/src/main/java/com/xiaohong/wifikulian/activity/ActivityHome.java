978b5ead0f822c6fb046e77438c670f5d143f32e
Lpoint <xiaoawl23@hotmail.com>
2017-03-10 17:52:08 +0800

    edit fragment connection

--- app/src/main/java/com/xiaohong/wifikulian/activity/ActivityHome.java
+++ app/src/main/java/com/xiaohong/wifikulian/activity/ActivityHome.java
@@ -1,5 +1,6 @@
 package com.xiaohong.wifikulian.activity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
