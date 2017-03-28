bc5fb4da57a1e2268d7ec5b4d9d25c819ba8f2c1
Lpoint <xiaoawl23@hotmail.com>
2017-03-28 15:22:39 +0800

    edit some layout

--- app/src/main/java/com/xiaohong/wifikulian/activity/ActivitySplash.java
+++ app/src/main/java/com/xiaohong/wifikulian/activity/ActivitySplash.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 
-import com.airbnb.lottie.LottieAnimationView;
+//import com.airbnb.lottie.LottieAnimationView;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.tencent.smtt.sdk.QbSdk;
 import com.xiaohong.wifikulian.Constants;
@@ -26,49 +26,53 @@ public class ActivitySplash extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        initView();
+//        initView();
 
+        Intent intent = new Intent();
+        intent.setClass(ActivitySplash.this, ActivityLogin.class);
+        startActivity(intent);
+        finish();
     }
 
     private void initView() {
-        final LottieAnimationView animationView = (LottieAnimationView) findViewById(R.id.animation_view);
-        animationView.addAnimatorListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                if (animationCancel)
-                    return;
-                Intent intent = new Intent();
-                intent.setClass(ActivitySplash.this, ActivityLogin.class);
-                startActivity(intent);
-                finish();
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-
-            }
-        });
-        animationView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                animationCancel = true;
-                animationView.cancelAnimation();
-                animationView.clearAnimation();
-                Intent intent = new Intent();
-                intent.setClass(ActivitySplash.this, ActivityLogin.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+//        final LottieAnimationView animationView = (LottieAnimationView) findViewById(R.id.animation_view);
+//        animationView.addAnimatorListener(new Animator.AnimatorListener() {
+//            @Override
+//            public void onAnimationStart(Animator animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationEnd(Animator animation) {
+//                if (animationCancel)
+//                    return;
+//                Intent intent = new Intent();
+//                intent.setClass(ActivitySplash.this, ActivityLogin.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//
+//            @Override
+//            public void onAnimationCancel(Animator animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animator animation) {
+//
+//            }
+//        });
+//        animationView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                animationCancel = true;
+//                animationView.cancelAnimation();
+//                animationView.clearAnimation();
+//                Intent intent = new Intent();
+//                intent.setClass(ActivitySplash.this, ActivityLogin.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
     }
 }
--- app/src/main/java/com/xiaohong/wifikulian/fragment/FragmentMine.java
+++ app/src/main/java/com/xiaohong/wifikulian/fragment/FragmentMine.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.facebook.drawee.view.SimpleDraweeView;
@@ -21,6 +22,9 @@ public class FragmentMine extends BaseFragment {
     private SimpleDraweeView imgProtrait;
     private TextView txtNickname;
     private TextView txtVersion;
+    private TextView txtCoin;
+    private LinearLayout linControl;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -30,15 +34,18 @@ public class FragmentMine extends BaseFragment {
         return view;
     }
 
-    private void initView(View view){
-        imgProtrait = (SimpleDraweeView)view.findViewById(R.id.img_portrait);
+    private void initView(View view) {
+        imgProtrait = (SimpleDraweeView) view.findViewById(R.id.img_portrait);
         txtNickname = (TextView) view.findViewById(R.id.txt_nickname);
         txtVersion = (TextView) view.findViewById(R.id.txt_version);
+        linControl = (LinearLayout) view.findViewById(R.id.rel_control);
+        txtCoin = (TextView) linControl.findViewById(R.id.txt_coin);
     }
 
-    private void initData(){
+    private void initData() {
         imgProtrait.setImageURI(Variable.loginBean.getIcon_url());
         txtNickname.setText(Variable.loginBean.getNick_name());
         txtVersion.setText("WIFI酷连 V" + Utils.getVersion() + "build" + Utils.getVersionCode());
+        txtCoin.setText(Variable.loginBean.getCoin_num() + "");
     }
 }
